791f24047885ea7571ddf928a95b260307ce73d6
lilblizzard <lilblizzard97@gmail.com>
2020-07-02 14:51:58 -0400

    non-functioning sparql query: ssl error

--- catalog-bot.rb
+++ catalog-bot.rb
@@ -1,6 +1,10 @@
 require "dotenv/load"
 require "twitter"
 
+require "rdf"
+require "net/http/persistent"
+require "sparql/client"
+
 client = Twitter::REST::Client.new do |config|
   config.consumer_key = ENV["CONSUMER_KEY"]
   config.consumer_secret = ENV["CONSUMER_SECRET"]
@@ -8,4 +12,14 @@ client = Twitter::REST::Client.new do |config|
   config.access_token_secret = ENV["ACCESS_TOKEN_SECRET"]
 end
 
-client.update("Up and running!")
+sparql = SPARQL::Client.new("https://sdbm.library.upenn.edu/sparql/sdbm/query")
+sdbm = RDF::Vocabulary.new "https://sdbm.library.upenn.edu/"
+
+query = sparql.select(:source).
+  where(
+  [:source, RDF.type, sdbm[:sources]]
+).limit(10)
+
+query.each_solution do |solution|
+  puts solution.inspect
+end

d969196ba2d2502f0fca5379d5cf061204b166f9
lilblizzard <lilblizzard97@gmail.com>
2020-07-06 15:50:07 -0400

    working sparql query

---
+++ Gemfile
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+source "https://rubygems.org"
+
+# gem "rails"
+gem 'dotenv'
+gem 'twitter'
+gem 'sparql-client'
+gem 'rdf'
--- catalog-bot.rb
+++ catalog-bot.rb
@@ -2,7 +2,6 @@ require "dotenv/load"
 require "twitter"
 
 require "rdf"
-require "net/http/persistent"
 require "sparql/client"
 
 client = Twitter::REST::Client.new do |config|
@@ -15,11 +14,27 @@ end
 sparql = SPARQL::Client.new("https://sdbm.library.upenn.edu/sparql/sdbm/query")
 sdbm = RDF::Vocabulary.new "https://sdbm.library.upenn.edu/"
 
-query = sparql.select(:source).
-  where(
-  [:source, RDF.type, sdbm[:sources]]
-).limit(10)
+# query = sparql.select(:source).
+#   where(
+#   [:source, RDF.type, sdbm[:sources]],
+#   [sdbm[:sources_date], RDF.value, :sources_date]
+#   ).limit(10)
 
-query.each_solution do |solution|
-  puts solution.inspect
+query2 = "
+  PREFIX xsd: <http://www.w3.org/2001/XMLSchema#>
+  PREFIX sdbm: <https://sdbm.library.upenn.edu/>
+
+  SELECT ?source ?source_date ?source_type WHERE {
+    #VALUES ?source_type {<https://sdbm.library.upenn.edu/source_types/1>} .
+    ?source a sdbm:sources ;
+      sdbm:sources_date ?source_date.
+  }
+"
+
+puts query2
+
+sparql.query(query2).each_solution do |solution|
+  puts solution[:source]
+  puts solution[:source_date]
 end
+

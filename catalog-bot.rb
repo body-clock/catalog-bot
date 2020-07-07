84bd96233546b872d998a522931776820fb0c86c
lilblizzard <lilblizzard97@gmail.com>
2020-07-07 15:59:12 -0400

    added date object to accessible data

--- catalog-bot.rb
+++ catalog-bot.rb
@@ -3,6 +3,7 @@ require "twitter"
 
 require "rdf"
 require "sparql/client"
+require "date"
 
 client = Twitter::REST::Client.new do |config|
   config.consumer_key = ENV["CONSUMER_KEY"]
@@ -18,25 +19,31 @@ month = time.month
 day = time.day
 month_day = sprintf "%02d%02d", month, day
 
-regex = 
-
-
 query = %Q[
   PREFIX xsd: <http://www.w3.org/2001/XMLSchema#>
   PREFIX sdbm: <https://sdbm.library.upenn.edu/>
 
-  SELECT ?source ?source_date ?source_type WHERE {
+  SELECT ?source ?source_date ?source_title WHERE {
     ?source a sdbm:sources ;
-    sdbm:sources_date ?source_date.
+    sdbm:sources_date ?source_date ;
+    sdbm:sources_title ?source_title .
 
     FILTER regex(?source_date,  "\\\\d{4,4}#{month_day}")
   }
 ]
 
-puts query
-
+sources = []
 sparql.query(query).each_solution do |solution|
-  puts solution[:source]
-  puts solution[:source_date]
+  data = {}
+  data[:title] = "#{solution[:source_title]}"
+  data[:link] = "#{solution[:source]}"
+  
+  data[:date] = Date.parse("#{solution[:source_date]}")
+  sources << data
 end
 
+random_source = sources[rand(0..sources.length)]
+
+tweet = "Check out #{random_source[:title]} released on #{random_source[:date]}! #{random_source[:link]}"
+puts tweet
+#client.update(tweet)
